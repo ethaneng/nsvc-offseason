581609e47d1c04d796dfb3d7409033c8663e58e1
Ethan Eng <ethaneng01@hotmail.com>
2024-01-09 14:48:02 +0800

    added animations to rego buttons

--- components/EventRegistration/RegistrationCardButtons.tsx
+++ components/EventRegistration/RegistrationCardButtons.tsx
@@ -1,15 +1,17 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../ui/button';
 import useSupabaseOnClient from '@/lib/hooks/useSupabaseOnClient';
 import { useRouter } from 'next/navigation';
-
-const supabase = useSupabaseOnClient();
+import { Loader2Icon, LoaderIcon } from 'lucide-react';
 
 export function RegisterCardButton({ rego_id, user_id }: { rego_id: number; user_id: string }) {
+	const supabase = useSupabaseOnClient();
 	const router = useRouter();
+	const [loading, setLoading] = useState(false);
 
 	async function register() {
+		setLoading(true);
 		const { error } = await supabase
 			.from('Participant')
 			.insert([{ registration_type: rego_id, user_id }])
@@ -20,13 +22,31 @@ export function RegisterCardButton({ rego_id, user_id }: { rego_id: number; user
 		}
 		router.refresh();
 	}
-	return <Button onClick={register}>Register</Button>;
+	return (
+		<Button
+			disabled={loading}
+			onClick={register}
+		>
+			Register{' '}
+			{loading && (
+				<span>
+					<Loader2Icon
+						size={16}
+						className="ml-2 animate-spin"
+					/>
+				</span>
+			)}
+		</Button>
+	);
 }
 
 export function UnregisterCardButton({ rego_id, user_id }: { rego_id: number; user_id: string }) {
+	const supabase = useSupabaseOnClient();
 	const router = useRouter();
+	const [loading, setLoading] = useState(false);
 
 	async function unregister() {
+		setLoading(true);
 		const { error } = await supabase
 			.from('Participant')
 			.delete()
@@ -43,7 +63,15 @@ export function UnregisterCardButton({ rego_id, user_id }: { rego_id: number; us
 			variant={'outline'}
 			onClick={unregister}
 		>
-			Unregister
+			Unregister{' '}
+			{loading && (
+				<span>
+					<Loader2Icon
+						size={16}
+						className="ml-2 animate-spin"
+					/>
+				</span>
+			)}
 		</Button>
 	);
 }
