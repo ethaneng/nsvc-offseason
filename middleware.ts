1abfab74d3a2239bb07a5055c575efd5be9093f3
Ethan Eng <ethaneng01@hotmail.com>
2024-01-23 15:24:31 +0800

    Removed the pathname cookie interceptor

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,7 @@ import type { NextRequest } from 'next/server';
 import type { Database } from '@/types/supabase';
 
 export async function middleware(req: NextRequest) {
-	const requestHeaders = new Headers(req.headers)
-	requestHeaders.set("x-pathname", req.nextUrl.pathname)
-
-	const res = NextResponse.next({request: {
-		headers: requestHeaders
-	}});
-	
+	const res = NextResponse.next();
 	const supabase = createMiddlewareClient<Database>({ req, res });
 	await supabase.auth.getSession();
 	return res;
