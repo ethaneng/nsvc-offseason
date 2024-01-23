b6216ecd24a12c3e4115fe9849b3c346c0df928a
Ethan Eng <ethaneng01@hotmail.com>
2024-01-23 15:39:33 +0800

    debug 2

--- components/Navigation/nav.tsx
+++ components/Navigation/nav.tsx
@@ -20,7 +20,7 @@ async function Nav() {
 		console.error(error);
 		return;
 	}
-	console.log('Got nav');
+	console.log('Got nav', session);
 	const { data: user, error: userError } = await supabase
 		.from('Users')
 		.select('*')
