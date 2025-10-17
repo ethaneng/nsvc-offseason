6a0c449605e2fc234fabaeb78262bd787c4fbd74
Ethan Eng <ethan@ethaneng.local>
2025-10-17 15:33:32 +0800

    fixed grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,34 @@
-import Grid from '@/components/animation/Grid';
-import { Button } from '@/components/ui/button';
-import { Calendar, Search } from 'lucide-react';
-import Link from 'next/link';
-import React from 'react';
+import Grid from "@/components/animation/Grid";
+import { Button } from "@/components/ui/button";
+import { Calendar, Search } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 function page() {
-	return (
-		<>
-			<div className="flex-1 relative justify-center items-center flex-col flex">
-				<Grid />
-			</div>
-			<div className="absolute top-0 left-0 flex flex-col justify-center items-center w-screen h-screen">
-				<h1 className="text-5xl font-bold max-w-2xl text-center">
-					A Platform for <span className="text-blue-600">Training Opportunities</span> and
-					<span className="text-blue-600"> Events</span>.
-				</h1>
-				<p className="max-w-lg text-center mt-4 text-neutral-200=">
-					Ran and managed by the Northern Stars Volleyball Club. Created by Ethan Eng.
-				</p>
-				<Button className="mt-4 group hover:bg-blue-700 hover:text-white">
-					<Link
-						href={'/home'}
-						className="flex items-center"
-					>
-						Check Upcoming Events
-						<Search
-							size={16}
-							className="ml-2 animate-bounce"
-						/>
-					</Link>
-				</Button>
-			</div>
-		</>
-	);
+  return (
+    <div className="flex-1 flex">
+      <div className="flex-1 relative justify-center items-center flex-col flex">
+        <Grid />
+      </div>
+      <div className="absolute top-0 left-0 flex flex-col justify-center items-center w-screen h-screen">
+        <h1 className="text-3xl md:text-5xl font-bold max-w-2xl text-center">
+          A Platform for{" "}
+          <span className="text-blue-600">Training Opportunities</span> and
+          <span className="text-blue-600"> Events</span>.
+        </h1>
+        <p className="max-w-lg text-sm md:text-base text-center mt-4 text-neutral-200=">
+          Ran and managed by the Northern Stars Volleyball Club. Created by
+          Ethan Eng.
+        </p>
+        <Button className="mt-4 group text-sm md:text-base hover:bg-blue-700 hover:text-white">
+          <Link href={"/home"} className="flex items-center">
+            Check Upcoming Events
+            <Search size={16} className="ml-2 animate-bounce" />
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default page;
